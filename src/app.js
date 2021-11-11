2214bf88bc99915b47dce1341c37ef83d56f136f
PMafra <pmafrarm@gmail.com>
2021-11-11 14:11:36 -0300

    Create sign-in route, send token for initialized session

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import signUp from './controllers/signUp.js';
+import signIn from './controllers/sign-in.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/sign-up', signUp);
+app.post('/sign-in', signIn);
 
 export default app;
