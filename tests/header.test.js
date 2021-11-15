f85dcd773f1aaa80e524c802389d1124f17a286f
Andrezza Souza <andrezzasouza@id.uff.br>
2021-11-15 02:22:50 -0300

    Pass 3 more tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,6 @@
 import '../src/setup.js';
 import supertest from 'supertest';
+import { v4 as uuid } from 'uuid';
 import app from '../src/app.js';
 import connection from '../src/database/database.js';
 import clearDatabase from './factories/tableFactory.js';
@@ -13,18 +14,20 @@ afterAll(() => {
 });
 
 describe('DELETE /header', () => {
-  // it('returns 401 when no token is sent', async () => {
-  //   const result = await supertest(app).delete('/header').set('Authorization', null);
-  //   expect(result.status).toEqual(401);
-  // });
+  it('returns 401 when no token is sent', async () => {
+    const result = await supertest(app).delete('/header');
+    expect(result.status).toEqual(401);
+  });
 
-  // it('returns 400 when token format is incorrect', async () => {
-  //   const result = await supertest(app).delete('/header').set('Authorization', badToken);
-  //   expect(result.status).toEqual(401);
-  // });
+  it('returns 400 when token format is incorrect', async () => {
+    const result = await supertest(app).delete('/header').set('Authorization', '156_87*95');
+    expect(result.status).toEqual(400);
+  });
 
   it('returns 410 when the sessions has already been deleted', async () => {
-    const result = await supertest(app).delete('/header');
+    const realToken = uuid();
+    // generate valid, but non-existant token
+    const result = await supertest(app).delete('/header').set('Authorization', realToken);
     expect(result.status).toEqual(410);
   });
 
