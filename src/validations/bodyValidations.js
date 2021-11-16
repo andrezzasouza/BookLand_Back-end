773ef9e20032f9a733524606422efb325966bad9
PMafra <pmafrarm@gmail.com>
2021-11-15 22:16:52 -0300

    Fix email not allowing '.com.br'

--- src/validations/bodyValidations.js
+++ src/validations/bodyValidations.js
@@ -11,7 +11,7 @@ const signUpSchema = Joi.object()
   .keys({
     name: Joi.string().min(1).max(30).required(),
     email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      .email()
       .required(),
     password: Joi.string().pattern(passwordRegex).required(),
     CPF: Joi.string().pattern(stringWithOnlyNumbers).length(11),
@@ -21,7 +21,7 @@ const signInSchema = Joi.object()
   .length(2)
   .keys({
     email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      .email()
       .required(),
     password: Joi.string().pattern(passwordRegex).required(),
   });
